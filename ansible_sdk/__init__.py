5cb7fed172d4081bc8edecc6e22d4719cb6c87dc
Abhijeet Kasurde <akasurde@redhat.com>
2022-08-02 12:50:15 +0530

    towncrier: For autogenerating changelog
    * added towncrier configuration file
    * added changelogs/fragments directory structure for storing
      changelog fragments
    
    Fixes: #3
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- ansible_sdk/__init__.py
+++ ansible_sdk/__init__.py
@@ -1,3 +1,5 @@
 from .model.job_def import AnsibleJobDef
 from .model.job_event import AnsibleJobEvent
 from .model.job_status import AnsibleJobStatus
+
+__version__ = '0.0.1'
---
+++ changelogs/fragments/3.feature
@@ -0,0 +1,1 @@
+Use towncrier for maintaining changelogs and news.
