7bf42d62fb5fea6e7a623e0df92a7a6e79de168c
Matt Davis <mrd@redhat.com>
2022-10-12 00:19:13 -0700

    share sample code across executors

---
+++ examples/example_common.py
@@ -0,0 +1,88 @@
+import asyncio
+
+from ansible_sdk import AnsibleJobDef
+
+
+async def run_one_stdout(executor, executor_options):
+    """
+    Run a single playbook job with several hosts and echo the display output as it arrives
+    """
+    try:
+        job_def = AnsibleJobDef(data_dir='datadir', playbook='pb.yml')
+        job_status = await executor.submit_job(job_def, executor_options)
+
+        async for line in job_status.stdout_lines:
+            print(line)
+
+        # directly await the job object
+        print('*** directly awaiting the job status...')
+        await job_status
+    finally:
+        print('all done, exiting')
+
+
+async def run_one_events(executor, executor_options):
+    """
+    Run a single playbook job with several hosts using the specified executor and options, and dump the raw event
+    output to the display as it arrives
+    """
+    try:
+        job_def = AnsibleJobDef(data_dir='datadir', playbook='pb.yml')
+        job_status = await executor.submit_job(job_def, executor_options)
+
+        eventcount = 0
+        async for ev in job_status.events:
+            eventcount += 1
+            print(f'*** consumed event {ev}')
+
+        print(f'event enumeration completed, total {eventcount}')
+
+        # directly await the job object
+        print('*** directly awaiting the job status...')
+        await job_status
+    finally:
+        print('all done, exiting')
+
+
+async def run_many(executor, executor_options):
+    """
+    Run five concurrent jobs (with slightly different definitions) against several hosts and dump the raw event
+    output to the display as it arrives (interleaving output between the jobs).
+    """
+    try:
+        job_def = AnsibleJobDef(data_dir='datadir', playbook='pb.yml')
+
+        num_jobs = 5
+
+        print(f'starting {num_jobs} jobs...')
+
+        # start all the jobs now
+        job_statuses = [
+            await executor.submit_job(
+            job_def.replace(extra_vars=dict(extra_var_value=f'job_{i}')), executor_options) for i in range(0, num_jobs)
+        ]
+
+        async def dump_events(job_status):
+            async for ev in job_status.events:
+                print(f'got event from job {job_status._job_def.extra_vars["extra_var_value"]}: {ev}')
+
+                # manually drop event after consumption
+                job_status.drop_event(ev)
+
+        # start one dumper per job to interleave the event output from all running jobs as the events arrive
+        dumpers = [asyncio.create_task(dump_events(s)) for s in job_statuses]
+
+        # FIXME: should probably await these?
+
+        print(f'waiting for jobs to complete...')
+        await asyncio.gather(*job_statuses)
+
+        # dump remaining events (shouldn't be any since we dropped them all after displaying them)
+        print('final event dump (should be none)')
+        for j in job_statuses:
+            async for ev in j.events:
+                print(f'got event {ev}')
+
+    finally:
+        print('all done, exiting')
+
--- examples/example_docker_job.py
+++ examples/example_docker_job.py
@@ -1,38 +1,17 @@
-
 import asyncio
 
-from ansible_sdk import AnsibleJobDef
 from ansible_sdk.executors import AnsibleDockerJobExecutor, AnsibleDockerJobOptions
+from example_common import run_one_stdout, run_one_events, run_many
 
 
 async def main():
     executor = AnsibleDockerJobExecutor()
-    jobdef = AnsibleJobDef('datadir', 'pb.yml')
-    options = AnsibleDockerJobOptions(image_ref='quay.io/ansible/ansible-runner:devel')
+    executor_options = AnsibleDockerJobOptions(container_image_ref='quay.io/ansible/ansible-runner:devel')
 
-    job_status = await executor.submit_job(jobdef, options)
+    await run_one_stdout(executor, executor_options)
+    await run_one_events(executor, executor_options)
+    await run_many(executor, executor_options)
 
-    # consume events and accumulate stdout replica
-    stdout = ''
-
-    # consume events as they arrive
-    eventcount = 0
-    async for ev in job_status.events:
-        eventcount += 1
-        print(f'*** consumed event {ev}')
-
-    print(f'event enumeration completed, total {eventcount}')
-
-    print(f'stdout results: {stdout}')
-
-    # directly await the job object
-    print('*** directly awaiting the job status...')
-    await job_status
-
-    print(f'job done? {job_status.done}')
-    print(f'event count: {len(job_status._events)}')
-
-    print('all done, exiting')
 
 if __name__ == '__main__':
     asyncio.run(main())
