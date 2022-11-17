e7a4b050e1aaf5867ca9ade3600a238614301c90
Abhijeet Kasurde <akasurde@redhat.com>
2022-11-17 20:09:02 +0530

    Update examples
    * Use job_options while running the job
    
    Fixes: #64
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- examples/example_podman_job.py
+++ examples/example_podman_job.py
@@ -7,10 +7,13 @@ from example_common import run_one_stdout, run_one_events, run_many
 async def main():
     executor = AnsiblePodmanJobExecutor()
     executor_options = AnsiblePodmanJobOptions(container_image_ref='quay.io/ansible/ansible-runner:devel')
+    job_options = {
+        'limit': 1
+    }
 
-    await run_one_stdout(executor, executor_options)
-    await run_one_events(executor, executor_options)
-    await run_many(executor, executor_options)
+    await run_one_stdout(executor, executor_options, job_options=job_options)
+    await run_one_events(executor, executor_options, job_options=job_options)
+    await run_many(executor, executor_options, job_options=job_options)
 
 
 if __name__ == '__main__':
--- examples/example_subprocess_job.py
+++ examples/example_subprocess_job.py
@@ -8,10 +8,11 @@ from example_common import run_one_stdout, run_one_events, run_many
 async def main():
     executor = AnsibleSubprocessJobExecutor()
     executor_options = AnsibleSubprocessJobOptions()
+    job_options = {}
 
-    await run_one_stdout(executor, executor_options)
-    await run_one_events(executor, executor_options)
-    await run_many(executor, executor_options)
+    await run_one_stdout(executor, executor_options, job_options=job_options)
+    await run_one_events(executor, executor_options, job_options=job_options)
+    await run_many(executor, executor_options, job_options=job_options)
 
 
 if __name__ == '__main__':
