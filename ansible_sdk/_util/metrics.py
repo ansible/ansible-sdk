adda198d272b32867726fcda97d9b634fee91d70
Abhijeet Kasurde <akasurde@redhat.com>
2023-02-16 17:53:01 +0530

    role data
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- ansible_sdk/_util/metrics.py
+++ ansible_sdk/_util/metrics.py
@@ -2,6 +2,8 @@
 # Apache License 2.0 (see LICENSE or https://www.apache.org/licenses/LICENSE-2.0)
 
 import os
+import uuid
+
 from datetime import datetime
 
 import ansible_sdk._util.dataclass_compat as dataclasses
@@ -32,30 +34,72 @@ class MetricsCalc:
     async def collect_metrics(self, status_obj: AnsibleJobStatus):
         metrics_data = {
             "task_counter": 0,
+            "task_data": {},
+            "role_data": {},
         }
 
         if not status_obj._job_def.metrics_output_path:
             return
 
+        # Create job.csv
+        job_id = uuid.uuid4()
+        job_csv_filename = os.path.join(
+            status_obj._job_def.metrics_output_path,
+            "job_%s.csv" % job_id,
+        )
+        job_csv_fh = open(job_csv_filename, "w+")
+        headers = [x.name for x in dataclasses.fields(AnsiblePlaybookStats)]
+        job_csv_writer = CSVAsync(job_csv_fh, headers, restval="NULL")
+        await job_csv_writer.writeheader_async()
+
+        # Create modules.csv
+        module_csv_filename = os.path.join(
+            status_obj._job_def.metrics_output_path,
+            "modules_%s.csv" % job_id,
+        )
+        module_csv_fh = open(module_csv_filename, "w+")
+        headers = ["job_id", "module_fqcn", "role_fqcn", "task_count", "task_duration"]
+        module_csv_writer = CSVAsync(module_csv_fh, headers, restval="NULL")
+        await module_csv_writer.writeheader_async()
+
         async for ev in status_obj.events:
+            end_time = datetime.strptime(ev.get("created"), "%Y-%m-%dT%H:%M:%S.%f")
             if ev.get("event") == "playbook_on_start":
                 metrics_data["started"] = datetime.strptime(
                     ev.get("created"), "%Y-%m-%dT%H:%M:%S.%f"
                 )
             if ev.get("event") == "playbook_on_task_start":
                 metrics_data["task_counter"] += 1
-            if ev.get("event") == "playbook_on_stats":
-                csv_filename = os.path.join(
-                    status_obj._job_def.metrics_output_path,
-                    "job_%s.csv" % ev["runner_ident"],
-                )
-                fh = open(csv_filename, "w+")
-                headers = [x.name for x in dataclasses.fields(AnsiblePlaybookStats)]
-                writer = CSVAsync(fh, headers, restval="NULL")
-                await writer.writeheader_async()
-                end_time = datetime.strptime(ev.get("created"), "%Y-%m-%dT%H:%M:%S.%f")
+                # Update the count of task
+                resolved_task_name = ev["event_data"]["resolved_action"]
+                if resolved_task_name in metrics_data["task_data"]:
+                    metrics_data["task_data"][resolved_task_name] += 1
+                else:
+                    metrics_data["task_data"][resolved_task_name] = 1
 
-                await writer.writerow_async(
+                # Update the count of role
+                resolved_role_name = ev["event_data"].get("role")
+                if resolved_role_name:
+                    if resolved_role_name in metrics_data["role_data"]:
+                        metrics_data["role_data"][resolved_role_name] += 1
+                    else:
+                        metrics_data["role_data"][resolved_role_name] = 1
+
+            if ev.get("event") == "playbook_on_stats":
+                # Write module metrics data
+                for task in metrics_data["task_data"]:
+                    await module_csv_writer.writerow_async(
+                        {
+                            "job_id": ev["runner_ident"],
+                            "module_fqcn": task,
+                            "role_fqcn": "",
+                            "task_count": metrics_data["task_data"][task],
+                            "task_duration": "",
+                        }
+                    )
+
+                # Write job metrics data
+                await job_csv_writer.writerow_async(
                     {
                         "job_id": ev["runner_ident"],
                         "job_type": "local",
@@ -68,10 +112,11 @@ class MetricsCalc:
                         "hosts_failed": len(ev["event_data"]["failures"]),
                         "hosts_unreachable": len(ev["event_data"]["failures"]),
                         "task_count": metrics_data["task_counter"],
-                        "task_duration": metrics_data.get("started") - end_time,
+                        "task_duration": end_time - metrics_data.get("started"),
                     }
                 )
-                fh.close()
-            print(ev)
 
             status_obj.drop_event(ev)
+
+        module_csv_fh.close()
+        job_csv_fh.close()
